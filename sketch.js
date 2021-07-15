8d6c2631681846b668166ba4dffb5d2a200742ee
FragilePackaging <86084817+FragilePackaging@users.noreply.github.com>
2021-07-15 19:49:51 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var path, pathImg;
+var Jake, jakeRun;
+function preload(){
+  //pre-load images
+  jakeRun= loadAnimation("Runner-1.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  var path = createSprite()
+}
+
+function draw() {
+  background(0);
+
+}
